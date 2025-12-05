01030b444362c60e65ffc5f978ac73f45ab16375
m2tavares@ucsd.edu <mtavaresloboesilva@sandiego.edu>
2025-12-05 15:22:40 -0800

    add all classes

---
+++ class04/class04.R
@@ -0,0 +1,14 @@
+#' ---
+#' title: "Class 4 Lab"
+#' author: "Maria Tavares (PID A69036242)"
+#' ---
+
+# This is my first R Script 
+
+x <- 1:50
+plot(x)
+plot(x, col="yellow")
+plot(x, sin(x), type = "l", col="red", lwd=3)
+log(10)
+log(10, base = 10)
+log(10,2)
